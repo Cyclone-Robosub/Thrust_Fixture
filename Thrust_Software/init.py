f2cfa6dcec133eec46743eacde582a3b0bd03ce8
Michael Tisdale <mltisdale@ucdavis.edu>
2026-05-06 18:57:31 -0700

    Created a .csv and made some modifications to the code.

--- Thrust_Software/init.py
+++ Thrust_Software/init.py
@@ -4,5 +4,5 @@ from hx711 import HX711                # import the class HX711
 GPIO.setmode(GPIO.BCM)                 # set GPIO pin mode to BCM numbering
 
 # initialize both amplifiers
-hx_1 = HX711(dout_pin=23, pd_sck_pin=24)
-hx_2 = HX711(dout_pin=27, pd_sck_pin=22)
+hx_1 = HX711(23, 24)
+hx_2 = HX711(27, 22)
