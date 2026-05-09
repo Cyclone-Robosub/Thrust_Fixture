984302c1b0033f6998662b58323dba577c2e78f9
Michael Tisdale <mltisdale@ucdavis.edu>
2026-05-08 23:29:04 -0700

    Added option for changing sampling frequency. Changed name of file outpt.

--- Thrust_Software/main.py
+++ Thrust_Software/main.py
@@ -7,21 +7,23 @@ from calib import hx_1, hx_2, offset_1, offset_2, ratio
 # set the sampling rate of the force values to 2 x frequency? Nyquist
 # add a PWM log?
 # Available PWM signal files --> these will need to be routed to computer's files?
-PWMs = ['square_wave_1700us_test.csv']
+PWMs = ['square_wave_1700us_test']
 
 # correct GPIO?
 ESC = PWM(Pin(13))
 
 # ESC frequency matches sampling rate
 ESC.freq(80)
+frequency = 80 
 
 # 65535 corresponds to the 16 bits of the Pico, the 12500 corresponds to the freqeuncy
-def set_throttle(pulse_us):
-    duty = int(pulse_us * 65535 / 12500)
+def set_throttle(pulse_us, frequency):
+    div = 10**6 / frequency
+    duty = int(pulse_us * 65535 / div)
     ESC.duty_u16(duty)
 
 # Arm the ESC
-set_throttle(1000)
+set_throttle(1000, frequency)
 time.sleep(2)
 
 # Display available signals
@@ -33,7 +35,6 @@ for i, name in enumerate(PWMs):
 signal_name = input("Enter PWM Signal Name: ")
 
 if signal_name in PWMs:
-    index = PWMs.index(signal_name)
     print(f"Selected: {signal_name}")
 else:
     print(f"Signal {signal_name} not found.")
@@ -45,11 +46,11 @@ data_2 = []
 pwm_log = []
 timestamps = []
 
-start_time = time.ticks_ms()
+#start_time = time.ticks_ms()
 
 # Main data collection loop
 try:
-    with open(signal_name, 'r') as f:
+    with open(signal_name + '.csv',  'r') as f:
         for line in f:
             col = line.strip().split(',')
             if len(col) < 2:
@@ -60,7 +61,7 @@ try:
             times = float(time_val)
             pwm = int(pwm_val)
 
-            set_throttle(pwm)
+            set_throttle(pwm, frequency)
 
             # take calibrated readings
             reading_1 = (hx_1.get_value() - offset_1) / ratio
@@ -77,12 +78,12 @@ except KeyboardInterrupt:
     print("\nStopped by user.")
 
 finally: 
-    set_throttle(1000)
+    set_throttle(1000, frequency)
     time.sleep(2)
     ESC.deinit()
     t = time.localtime()
     timestamp = '{}-{:02d}-{:02d}-{:02d}-{:02d}-{:02d}'.format(t[0], t[1], t[2], t[3], t[4], t[5])
-    filename = f'{signal_name}.csv'
+    filename = f'{signal_name}-{timestamp}.csv'
 
     with open(filename, 'w') as f:
         f.write('time_ms,loadcell_1, loadcell_2, pwm\n')
